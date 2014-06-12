264761dfc99702ecee57484d679b8edff9e53eca
Sjoerd Nijhof <sjoerdnijhof@hotmail.com>
2014-06-12 15:44:37 +0200

    Added ASP version

---
+++ Sudoku-WPF/Sudoku/SudokuASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SudokuASP.MvcApplication" Language="C#" %>
---
+++ Sudoku-WPF/Sudoku/SudokuASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SudokuASP.Startup))]
+namespace SudokuASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
